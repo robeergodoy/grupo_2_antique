3cc109ed83d21f4f06636319dedae3312aa3f9b4
Facundo <facundolanus@gmail.com>
2020-01-17 19:07:16 -0300

    un par de cosas mas...

--- app/src/controllers/productsController.js
+++ app/src/controllers/productsController.js
@@ -1,7 +1,10 @@
 const fs = require('fs')
-let productsList = fs.readFileSync('./data/dataBase.json', {encoding: 'utf8'})
-productsList = JSON.parse(productsList)
+const productsDbPath = './data/dataBase.json'
 
+
+let productsList = fs.readFileSync(productsDbPath, {encoding: 'utf8'})
+
+productsList = JSON.parse(productsList)
 
 function binarySearch(arrayObjetc, first, last, id) {
     var mid = Math.floor((last - first)/2 + first)
@@ -27,20 +30,37 @@ const controller = {
     products: (req,res) => {
         res.render('products', {productsList: productsList})
     },
- 
-    productDetail: (req, res) => {
 
+    productCreate: (req, res) => {
+        res.render('productCreate')
+    },
+
+    productDetail: (req, res) => {
         let productIndex = binarySearch(productsList, 0, productsList.length - 1, req.params.id)
 
         res.render('productDetail', {product: productsList[productIndex]})
     },
-    
+
+    productCreateSave: (req, res) => {
+        let newProduct = req.body
+
+        newProduct.id = ((productsList[productsList.length - 1]).id) + 1
+
+/* Falt validar que sucede cuando no queremos subir ninguna foro */
+
+        newProduct.imageName = req.files[0].filename
+
+        newProduct.category = ""
+        
+        productsList.push(newProduct)
+        productsList = JSON.stringify(productsList)
+        fs.writeFileSync(productsDbPath, productsList)
+
+        res.redirect('/')
+    },
+  
     productCart: (req,res) => {
 
-    },
-
-    productCreate: (req, res) => {
-        res.send('productAdd')
     },
 
     productEdit: (req, res) => {
--- app/src/routes/products.js
+++ app/src/routes/products.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const router = express.Router()
-
+const multer = require('multer')
+const path = require('path')
 const productsController = require('../controllers/productsController')
+const destinationImgPath = 'public/images/'
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, destinationImgPath)
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+var upload = multer({storage: storage})
+
 
 router.get('/', productsController.products)
-
-router.get('/:id', productsController.productDetail)
+router.post('/', upload.any(), productsController.productCreateSave)
 
 router.get('/create', productsController.productCreate)
+
+router.get('/:id', productsController.productDetail)
 
 router.get('/:id/edit', productsController.productEdit)
 
