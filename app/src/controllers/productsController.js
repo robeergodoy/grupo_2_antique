cb37eaefebf02519abd51d8f23e626f2c1b53b59
Gabriel Luque <gldidonna@gmail.com>
2020-03-23 16:46:39 -0300

    Agregado logica de db a products

--- app/src/controllers/productsController.js
+++ app/src/controllers/productsController.js
@@ -7,26 +7,6 @@ let productsList = fs.readFileSync(productsDbPath, {encoding: 'utf8'})
 
 productsList = JSON.parse(productsList)
 
-function binarySearch(arrayObjetc, first, last, id) {
-    var mid = Math.floor((last - first)/2 + first)
-
-    if (first > last) {
-        return -1
-    }
-
-    if (arrayObjetc[mid].id == id) {
-        return mid
-    }
-    
-    if (arrayObjetc[mid].id > id){
-        return binarySearch(arrayObjetc, first, mid - 1, id)
-    }
-
-    if (arrayObjetc[mid].id < id) {
-        return binarySearch(arrayObjetc, mid + 1, last, id)
-    }
-}
-
 const controller = {    
     products: (req,res, next) => {
         res.render('products', {productsList})
@@ -71,14 +51,33 @@ const controller = {
     },
 
     productEdit: (req, res) => {
-        let productIndex = binarySearch(productsList, 0, productsList.length - 1, req.params.id)
-        res.render('productEdit', {product: productsList[productIndex], isProductForEdit : true })
+        
+        db.product.findByPk(req.params.id,{
+			include: [
+				{
+					model: db.images
+				}
+			]
+		}).then(product => {
+			res.render('productEdit', {product: product, isProductForEdit : true })
+		})
     },
 
     productSave: (req, res) => {
-        console.log(".......................................v",req.body);
-        
-        res.redirect(`/products/${req.params.id}`)
+
+        console.log("Product body",req.body);
+        db.product.update(
+            {title: req.body.title,
+             price: req.body.price,
+             description: req.body.description},
+            {where: {
+                id:  req.params.id
+                }
+            }
+        )
+        .then( () => {
+            res.redirect(`/products/${req.params.id}`)
+        })
     }
 }
 
